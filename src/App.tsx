d1e4ddff74596a428d414fe6f0f3e194430bde59
omurilobueno <mwrilo@icloud.com>
2026-04-16 21:47:09 -0300

    login atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import Lista from "./paginas/Lista";
 import Login from "./paginas/Login";
 import Cadastro from "./paginas/Cadastro";
 import NotFound from "./paginas/NotFound";
+import Contador from "./paginas/contador";
+import Props from "./paginas/props";
 import "./App.css";
-
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/lista" element={<Lista />} />
       <Route path="/login" element={<Login />} />
       <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/lista/:nome" element={<Lista />} />
+      <Route path="/lista" element={<Lista />} />
+      <Route path="/contador" element={<Contador />} />
       <Route path="*" element={<NotFound />} />
-         </Routes>
+      <Route path="/props" element={<Props />} />
+    </Routes>
   );
 }
 
